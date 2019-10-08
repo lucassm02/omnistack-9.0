b6639aa6a6386878b4b38f069f7886c3f81eb04c
Lucas Santos <lucassm02@gmail.com>
2019-10-07 23:11:58 -0300

    Finalizado projeto, integracao entre as plataformas concluida

--- api/src/server.js
+++ api/src/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const routes = require("./routes");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
-
+const socketIo = require("socket.io");
+const http = require("http");
 require("dotenv/config");
 
 const app = express();
+const server = http.Server(app);
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
@@ -16,9 +18,25 @@ mongoose.connect(
   }
 );
 
+const io = socketIo(server);
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 app.use(cors());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
---
+++ mobile/src/pages/List.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import socketIo from "socket.io-client";
+import {
+  ScrollView,
+  Alert,
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  AsyncStorage
+} from "react-native";
+
+import SpotList from "../components/SpotList";
+
+import logo from "../assets/logo.png";
+
+export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user_id => {
+      const socket = socketIo("http://192.168.1.8:3333", {
+        query: {
+          user_id
+        }
+      });
+
+      socket.once("booking_response", booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} na data ${booking.date} foi ${
+            booking.approved ? "APROVADA" : "REJEITADA"
+          }`
+        );
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    AsyncStorage.getItem("techs").then(storagedTechs => {
+      const techsArray = storagedTechs.split(",").map(item => item.trim());
+
+      setTechs(techsArray);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 40
+  }
+});
